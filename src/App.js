7d4aefd829d7cc96c45c7f60794c69bd79e258f5
Groeneveld, Pim (SPLHZ) - KLM <groeneveld.pim@klm.com>
2020-03-25 17:54:17 +0100

    some initial work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MapContainer from './views/charity_map_view';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <Router>
+            <div class="topnav">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/map">Near me</Link>
+                </li>
+              </ul>
+              <hr />
+              <Route exact path="/map" component={MapContainer} />
+
+              <a
+                onClick={() => {
+                  window.location.href = 'charity_map_view.js';
+                }}
+              >
+                Go to maps
+              </a>
+            </div>
+          </Router>
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Just some text here about the greatest app to ever be created
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+            </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
